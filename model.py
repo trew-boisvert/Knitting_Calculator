c669d4050440ee65f70c532c888026fc42aa8cd5
Trew Boisvert <trewboisvert@gmail.com>
2021-02-09 02:05:29 +0000

    Scaffold ProjectRecord and PatternLibrary classes in model.py

--- model.py
+++ model.py
@@ -19,6 +19,7 @@ class User(db.Model):
 
         return f"<User_id= {self.user_id} user_name= {self.user_name}>"
 
+
 class ProjectRecord(db.Model):
     """Date model for a project record."""
 
@@ -35,6 +36,38 @@ class ProjectRecord(db.Model):
     current_row = db.Column(db.Integer, nullable=False)
     current_index = db.Column(db.Integer, nullable=False)
 
+    def __repr__(self):
+        return f"<Project id= {self.project_id} Project name= {self.project_name}>"
 
 
+class PatternLibrary(db.Model):
+    """Data model for stitch patterns."""
 
+    __tablename__ = "patterns"
+
+    pattern_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    pattern_name = db.Column(db.String(100), nullable=False)
+    pattern_description = db.Column(db.String(), nullable=False)
+    pattern_instructions = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Pattern id= {self.pattern_id} Patter name= {self.pattern_name}>"
+
+# change name of postgres file to something for my project, then uncomment function
+# def connect_to_db(app):
+#     """Connect the database to our Flask app."""
+
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///animals'
+#     app.config['SQLALCHEMY_ECHO'] = False
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#     db.app = app
+#     db.init_app(app)        
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+    print('Connected to db!')
+
+
+#add foreign keys
+#add relationships
