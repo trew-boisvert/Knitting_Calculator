dcdb2711429f0a71bf168939d00f54c78719f9f6
Trew Boisvert <trewboisvert@gmail.com>
2021-02-09 23:05:06 +0000

    Homepage function in server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, jsonify, render_template
+from model import connect_to_db, Human
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Show the homepage."""
+
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
