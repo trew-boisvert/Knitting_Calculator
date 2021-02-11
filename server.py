57d32ef4b5a3aaf0a0da36896918e279c7da4a0f
Trew Boisvert <trewboisvert@gmail.com>
2021-02-11 13:38:14 +0000

    Add routes to server.py

--- server.py
+++ server.py
@@ -10,10 +10,34 @@ app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
-def homepage():
-    """Show the homepage."""
+def welcome_page():
+    """Show the welcome/homepage."""
 
-    return render_template('base.html')
+    return render_template('welcome.html')
+
+@app.route('/login')
+def login_page():
+    """View login page."""
+
+    return render_template('login.html')
+
+@app.route('/calculator')
+def calculator_page():
+    """View calculator page."""
+
+    return render_template('calculator.html')
+
+@app.route('/profile')
+def profile_page():
+    """View profile page."""
+
+    return render_template('profile.html')
+
+@app.route('/photos')
+def photos_page():
+    """View photos page."""
+
+    return render_template('photos.html')
 
 
 if __name__ == '__main__':
