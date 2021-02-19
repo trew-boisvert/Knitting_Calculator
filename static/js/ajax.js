068b728b7e51fa3d4347f6602fbf601373bf3a11
Trew Boisvert <trewboisvert@gmail.com>
2021-02-19 12:38:42 +0000

    Refactor ajax.js

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,6 +1,8 @@
 var selectedProjectInstructions = {};
 var currentRow = 0
-function MakePatternFromInput(evt){
+
+
+$("#start-project").on('submit', (evt) => {
     evt.preventDefault();
 
     const formData = {
@@ -11,10 +13,10 @@ function MakePatternFromInput(evt){
         pHeight: $("#project-height").val()
     }
     let url = `/api/instructions`
-    console.log('stuff bottons oh no')
+
     $('#start-knitting').append('<button id="advance-row">Start</button>');
     $("#advance-row").on('click', (evt) => {
-        console.log('did it work?')
+        
         while(currentRow < selectedProjectInstructions.totalrows){
             for(let i = 0; i < selectedProjectInstructions.knitInstructions.length; i++){
                 $('#start-knitting').append(`<ul>Row ${currentRow + 1}</ul><ul>${selectedProjectInstructions.knitInstructions[i]}</ul>`)   
@@ -22,17 +24,17 @@ function MakePatternFromInput(evt){
             }
         }
     })
-    selectedProjectInstructions = {'totalrows': 32,
+    selectedProjectInstructions = {'totalrows': 12,
                                     'knitInstructions': ['line 1', 'line 2', 'line 3', 'line 4']}
-    // $.post(url, formData, (res) => {
-    //     console.log(res)
-    //     $('#stitchname').html(res['pattern_name']);
-    //     $('#description').html(res['pattern_description']);
-    //     $('#caston').html(res['cast_on']);
-    //     $('#totalrows').html(res['row_total']);
-    //     $('#knitpat').html(res['start']);
-    //     //reassign value of empty object variable
-    // })
-}
+    $.post(url, formData, (res) => {
+        console.log(res)
+        $('#stitchname').html(res['pattern_name']);
+        $('#description').html(res['pattern_description']);
+        $('#caston').html(res['cast_on']);
+        $('#totalrows').html(res['row_total']);
+        $('#knitpat').html(res['start']);
+        //reassign value of empty object variable
+    })
+});
   
-$("#start-project").on('submit', MakePatternFromInput)
+
