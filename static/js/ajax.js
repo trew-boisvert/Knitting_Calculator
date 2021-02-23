76670e3ba920b8a03b829a0486d15eb531a29e22
Trew Boisvert <trewboisvert@gmail.com>
2021-02-23 01:42:00 +0000

    Calculator complete, session data loads properly

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,7 +1,7 @@
 
- function makePatternFromInput(evt){
+ $("#start-project").on('submit', (evt) => {
     evt.preventDefault();
-    console.log('formData:', formData);
+    
     const formData = {
         patID: $("#pattern-id").val(),
         sWidth: $("#swatch-width").val(),
@@ -9,12 +9,31 @@
         pWidth: $("#project-width").val(),
         pHeight: $("#project-height").val()
     }
+    console.log('formData:', formData);
     let url = `/api/instructions`;
 //maybe change append to prepend
     $('#start-knitting').append('<button id="advance-row">Next Row</button>');
-    console.log(sessionStorage.stitchInstructions);
-    let stitch = sessionStorage.stitchInstructions.split(".,");
+    
+    $.post(url, formData, (res) => {
+        console.log(res);
+        $('#stitchname').html(res['pattern_name']);
+        $('#description').html(res['pattern_description']);
+        $('#caston').html(res['cast_on']);
+        $('#totalrows').html(res['row_total']);
+        $('#knitpat').html(res['start']);
+        
+        sessionStorage.setItem('row_total', res['row_total']);
+        sessionStorage.setItem('stitchInstructions', res['stitch'])
+        sessionStorage.setItem('currentRow', 0)
+        sessionStorage.setItem('indexer', 0)
+    })
+
+
     $("#advance-row").on('click', (evt) => {
+
+        let stitch = sessionStorage.getItem('stitchInstructions').split(".,");
+        console.log(sessionStorage.getItem('stitchInstructions'));
+
         console.log('current row', sessionStorage.currentRow);
         console.log('row total', sessionStorage.row_total);
         if(parseInt(sessionStorage.currentRow) === parseInt(sessionStorage.row_total)){
@@ -29,21 +48,5 @@
             sessionStorage.setItem('indexer', parseInt(sessionStorage.indexer) + 1);
         }
     })
-
-    $.post(url, formData, (res) => {
-        console.log(res);
-        $('#stitchname').html(res['pattern_name']);
-        $('#description').html(res['pattern_description']);
-        $('#caston').html(res['cast_on']);
-        $('#totalrows').html(res['row_total']);
-        $('#knitpat').html(res['start']);
-        
-        sessionStorage.setItem('row_total', res['row_total']);
-        sessionStorage.setItem('stitchInstructions', res['stitch'])
-        sessionStorage.setItem('currentRow', 0)
-        sessionStorage.setItem('indexer', 0)
-    })
-}
-
-$("#start-project").on('submit', makePatternFromInput)
+});
   
