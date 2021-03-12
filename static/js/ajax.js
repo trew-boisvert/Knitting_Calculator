f1d8a19c643716466eb697511803e54f3638f94a
Trew Boisvert <trewboisvert@gmail.com>
2021-03-12 07:10:52 +0000

    Style - text update on calc and login

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -35,14 +35,15 @@ $("#start-project").on('submit', (evt) => {
         let stitch = sessionStorage.getItem('stitchInstructions').split(".,");
 //maybe change append to prepend?
         if(parseInt(sessionStorage.currentRow) === parseInt(sessionStorage.row_total)){
-            $('#start-knitting').append(`<ul>That's all!  Cast off and you're done!</ul>`);
+            $('#start-instructions').prepend(`<ul>That's all!  Cast off and you're done!</ul>`);
+            $('#advance-row').hide();
         }
         else{
 //TODO bind numbers to variable and use that instead of parseing all the time
             if(parseInt(sessionStorage.indexer) === stitch.length){
                 sessionStorage.setItem('indexer', 0);
             }
-            $('#start-knitting').append(`<ul>Row ${parseInt(sessionStorage.currentRow) + 1}</ul><ul>${stitch[parseInt(sessionStorage.indexer)]}</ul>`)
+            $('#start-instructions').prepend(`<ul>Row ${parseInt(sessionStorage.currentRow) + 1}</ul><ul>${stitch[parseInt(sessionStorage.indexer)]}</ul>`)
             sessionStorage.setItem('currentRow', parseInt(sessionStorage.currentRow) + 1);
             sessionStorage.setItem('indexer', parseInt(sessionStorage.indexer) + 1);
         }
